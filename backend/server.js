bab7e113214cb511825934fa4aacf112fe2a9841
saiprakash <saiprakashraokotla@gmail.com>
2026-04-23 13:44:07 +0530

    display message for render live

--- backend/server.js
+++ backend/server.js
@@ -60,4 +60,9 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('WatchParty Hub backend is running');
+});
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
