b5be2d1ea2bafe783c34af2e1919982c602fef49
saiprakash <saiprakashraokotla@gmail.com>
2026-04-23 17:07:53 +0530

    Fix: Socket URL

--- frontend/watchparty-frontend/src/socket.js
+++ frontend/watchparty-frontend/src/socket.js
@@ -3,8 +3,9 @@ import { io } from 'socket.io-client';
 const baseURL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5000'
-    : 'https://watchparty-backend.onrender.com';
+    : 'https://watchparty-hub.onrender.com';
 
 export const socket = io(baseURL, {
-  autoConnect: false
+  autoConnect: false,
+  withCredentials: true
 });
